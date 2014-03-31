e340a6128b6e67fafe4b9fe1d0fb8c7e92447368
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-03-30 21:51:19 -0300

    implementing random phrases

---
+++ frases.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+frases = [
+    u"VAI TE COPA SIM!",
+    u"VAI TER TEM QUE TER",
+    u"VAI TER E SE ENCHER O SACO VAI TER DUAS",
+    u"VAI TER MAS SÓ ATÉ MEIO DIA",
+]
--- vaiter.py
+++ vaiter.py
@@ -1,11 +1,16 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template
+from frases import frases
+from random import shuffle
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    shuffle(frases)
+    return render_template('home.html', frase=frases[0])
 
 
 if __name__ == '__main__':
